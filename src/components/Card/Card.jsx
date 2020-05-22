64396324e38e738067cbfc8ce9cf0671c84dbb87
Fedor-Ivanov <fedorhim@gmail.com>
2020-05-22 13:44:14 +0300

    finish basic cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -12,80 +12,101 @@ function Card({ card }) {
     return (
         <div className='card'>
 
-
-            <p>{card.name}</p>
-
+            {/* img */}
             {<img className='card-img' src={require(`../../images/cards/${card.artid}.jpg`)} alt="card" />}
 
-
-            {card.color == 'Gold' &&
+            {/* border */}
+            {card.color === 'Gold' &&
                 <img className='card-border' src={require(`../../images/border/border-gold.png`)} alt="" />
             }
-            {card.color == 'Bronze' &&
+            {card.color === 'Bronze' &&
                 <img className='card-border' src={require(`../../images/border/border-bronze.png`)} alt="" />
             }
 
-
-            {card.faction == 'Neutral' &&
+            {/* banner */}
+            {card.faction === 'Neutral' &&
                 <img className='card-banner' src={require(`../../images/banner/banner-nt.png`)} alt="" />
             }
-            {card.faction == 'Monster' &&
+            {card.faction === 'Monster' &&
                 <img className='card-banner' src={require(`../../images/banner/banner-mo.png`)} alt="" />
             }
-            {card.faction == 'Skellige' &&
+            {card.faction === 'Skellige' &&
                 <img className='card-banner' src={require(`../../images/banner/banner-sk.png`)} alt="" />
             }
-            {card.faction == 'Northern Realms' &&
+            {card.faction === 'Northern Realms' &&
                 <img className='card-banner' src={require(`../../images/banner/banner-nr.png`)} alt="" />
             }
-            {card.faction == 'Syndicate' &&
+            {card.faction === 'Syndicate' &&
                 <img className='card-banner' src={require(`../../images/banner/banner-sy.png`)} alt="" />
             }
-            {card.faction == 'Nilfgaard' &&
+            {card.faction === 'Nilfgaard' &&
                 <img className='card-banner' src={require(`../../images/banner/banner-ng.png`)} alt="" />
             }
-            {card.faction == 'Scoiatael' &&
+            {card.faction === 'Scoiatael' &&
                 <img className='card-banner' src={require(`../../images/banner/banner-st.png`)} alt="" />
             }
 
-            {card.faction == 'Neutral' && +(card.provision) >= 1 &&
+            {/* provision */}
+            {card.faction === 'Neutral' && +(card.provision) >= 1 &&
                 <img className='card-provision' src={require(`../../images/provision/provision-nt.png`)} alt="" />
             }
-            {card.faction == 'Monster' && +(card.provision) >= 1 &&
+            {card.faction === 'Monster' && +(card.provision) >= 1 &&
                 <img className='card-provision' src={require(`../../images/provision/provision-mo.png`)} alt="" />
             }
-            {card.faction == 'Skellige' && +(card.provision) >= 1 &&
+            {card.faction === 'Skellige' && +(card.provision) >= 1 &&
                 <img className='card-provision' src={require(`../../images/provision/provision-sk.png`)} alt="" />
             }
-            {card.faction == 'Northern Realms' && +(card.provision) >= 1 &&
+            {card.faction === 'Northern Realms' && +(card.provision) >= 1 &&
                 <img className='card-provision' src={require(`../../images/provision/provision-nr.png`)} alt="" />
             }
-            {card.faction == 'Syndicate' && +(card.provision) >= 1 &&
+            {card.faction === 'Syndicate' && +(card.provision) >= 1 &&
                 <img className='card-provision' src={require(`../../images/provision/provision-sy.png`)} alt="" />
             }
-            {card.faction == 'Nilfgaard' && +(card.provision) >= 1 &&
+            {card.faction === 'Nilfgaard' && +(card.provision) >= 1 &&
                 <img className='card-provision' src={require(`../../images/provision/provision-ng.png`)} alt="" />
             }
-            {card.faction == 'Scoiatael' && +(card.provision) >= 1 &&
+            {card.faction === 'Scoiatael' && +(card.provision) >= 1 &&
                 <img className='card-provision' src={require(`../../images/provision/provision-st.png`)} alt="" />
             }
 
-
-            {card.rarity == 'Legendary' &&
+            {/* rarity */}
+            {card.rarity === 'Legendary' &&
                 <img className='card-rarity' src={require(`../../images/rarity/rarity-legendary.png`)} alt="" />
             }
-            {card.rarity == 'Epic' &&
+            {card.rarity === 'Epic' &&
                 <img className='card-rarity' src={require(`../../images/rarity/rarity-epic.png`)} alt="" />
             }
-            {card.rarity == 'Rare' &&
+            {card.rarity === 'Rare' &&
                 <img className='card-rarity' src={require(`../../images/rarity/rarity-rare.png`)} alt="" />
             }
-            {card.rarity == 'Common' &&
+            {card.rarity === 'Common' &&
                 <img className='card-rarity' src={require(`../../images/rarity/rarity-common.png`)} alt="" />
             }
 
+            {/* provision-icon */}
             {+(card.provision) >= 1 &&
                 <img className='card-provision-icon' src={require(`../../images/provision-icon.png`)} alt="" />
+            }
+
+            {/* provision-value */}
+            {+(card.provision) >= 1 &&
+                <span className='card-provision-value'>{card.provision}</span>
+            }
+
+            {/* power-value */}
+            {+(card.power) >= 1 &&
+                <span className='card-power-value'>{card.power}</span>
+            }
+
+            {/* type */}
+            {card.type == 'Special' &&
+                <img className='card-type' src={require(`../../images/trinket/trinket-spell.png`)} alt="" />
+            }
+            {card.type == 'Artifact' &&
+                <img className='card-type' src={require(`../../images/trinket/trinket-artifact.png`)} alt="" />
+            }
+            {card.type == 'Stratagem' &&
+                <img className='card-type' src={require(`../../images/trinket/trinket-stratagem.png`)} alt="" />
             }
 
 
